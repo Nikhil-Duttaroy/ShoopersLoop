372cc66161471e8b93387f102dfc839c5860345b
Nikhil Duttaroy <54467400+Nikhil-Duttaroy@users.noreply.github.com>
2021-03-23 18:41:30 +0530

    Navbar / Header Added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import {Route ,Switch} from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/HomePage/HomePage.components.jsx';
 import ShopPage from './pages/Shop/ShopPage.components.jsx';
+import Header from './components/Header/Header.components.jsx'
 
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>
