c81a82e0e33923fb43a96e717e8bd0db429b8345
Nikhil Duttaroy <54467400+Nikhil-Duttaroy@users.noreply.github.com>
2021-03-24 20:08:05 +0530

    Sign in and Sign up page added

---
+++ src/pages/SignInAndSignUp/SignInAndSignUp.components.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './SignInAndSignUp.styles.scss'
+import SignIn from './../../components/SignIn/SignIn.components';
+
+const SignInAndSignUp = () => {
+    return (
+        <div>
+            <SignIn/>
+        </div>
+    );
+}
+
+export default SignInAndSignUp;
