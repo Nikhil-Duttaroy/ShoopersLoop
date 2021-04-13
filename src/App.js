898fcd3efd84dffd2e5e2302c7264db761133fac
Nikhil Duttaroy <54467400+Nikhil-Duttaroy@users.noreply.github.com>
2021-04-13 14:09:35 +0530

    implemented state management using redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch,Redirect } from "react-router-dom";
+import {connect} from 'react-redux'
+
 
 import "./App.css";
 import Header from "./components/Header/Header.components.jsx";
@@ -8,15 +10,17 @@ import ShopPage from "./pages/Shop/ShopPage.components.jsx";
 import SignInAndSignUp from "./pages/SignInAndSignUp/SignInAndSignUp.components";
 
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  //** As redux is Used
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      currentUser: null,
-    };
-  }
+  //   this.state = {
+  //     currentUser: null,
+  //   };
+  // }
 
   unsubscribeFromAuth = null;
   componentDidMount() {
@@ -24,17 +28,27 @@ class App extends React.Component {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot((snapShot) => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data(),
-            }
-          }//,()=> {console.log(this.state);} 
-          );
+          //** As redux is Used
+          // this.setState(
+          //   {
+          //     currentUser: {
+          //       id: snapShot.id,
+          //       ...snapShot.data(),
+          //     },
+          //   } //,()=> {console.log(this.state);}
+          // );
           // console.log(this.state);
+
+          this.props.setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
+          });
+
         });
-        
-      } else this.setState({ currentUser: userAuth });
+      } else {
+        // this.setState({ currentUser: userAuth });
+        this.props.setCurrentUser(userAuth);
+      }
     });
   }
 
@@ -45,15 +59,24 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />{" "}
+        {/* currentUser={this.state.currentUser}  this is removed from header after using redux */}
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
-          <Route exact path='/signin' component={SignInAndSignUp} />
+          <Route exact  path='/signin'   render={()=>this.props.currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUp/>)  } />
         </Switch>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser, //state.user.currentUser  ----> Root reducer [user] =>userReducer[currentUser]
+});
+
+const mapDispatchToProps = dispatch=>({
+  setCurrentUser:user=>dispatch(setCurrentUser(user))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
