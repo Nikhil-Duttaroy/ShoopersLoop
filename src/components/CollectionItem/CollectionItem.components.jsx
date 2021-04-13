ae49f34fe4edb8973678209e4fda8fa4561f9a25
Nikhil Duttaroy <54467400+Nikhil-Duttaroy@users.noreply.github.com>
2021-04-13 19:02:20 +0530

    updated shop data,and cart utils to group same cart items

--- src/components/CollectionItem/CollectionItem.components.jsx
+++ src/components/CollectionItem/CollectionItem.components.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './CollectionItem.styles.scss'
 
+import { connect } from "react-redux";
 
-const CollectionItem=({id,name,price,imageUrl}) =>(
+import CustomButton from './../CustomButton/CustomButton.components';
+import { addItem } from '../../redux/cart/cart.actions';
+
+
+const CollectionItem=({item,addItem}) =>{
+    const { name, price, imageUrl }=item;
+    return(
     <div className="collection-item">
         <div className="image" loading="lazy"
         style={{backgroundImage: `url(${imageUrl})`}}
@@ -11,8 +18,12 @@ const CollectionItem=({id,name,price,imageUrl}) =>(
         <span className="name">{name}</span>
         <span className="price">${price}</span>
     </div>
+    <CustomButton onClick={()=>addItem(item)} inverted>Add To Cart</CustomButton>
     </div>
-)
+)}
 
+const mapDispatchToProps = (dispatch)=>({
+    addItem:item=>dispatch(addItem(item))
+})
 
-export default CollectionItem;
+export default connect(null,mapDispatchToProps)(CollectionItem);
