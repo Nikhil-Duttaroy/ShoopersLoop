b939af0e80a50f166d7c5da87fc41abc2fa5da7f
Nikhil Duttaroy <54467400+Nikhil-Duttaroy@users.noreply.github.com>
2021-04-16 19:34:40 +0530

    Sign Out Capitalized

--- src/components/Header/Header.components.jsx
+++ src/components/Header/Header.components.jsx
@@ -30,7 +30,7 @@ const Header = ({ currentUser ,hidden}) => (
       {currentUser ? (
         <div className='option' onClick={() => auth.signOut()}>
           {" "}
-          Sign Out
+          SIGN OUT
         </div>
       ) : (
         <Link className='option' to='/signin'>
