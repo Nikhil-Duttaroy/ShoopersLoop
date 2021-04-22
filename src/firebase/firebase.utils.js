20256069a96ae9003fbbde3934fc36d6a6cfb52d
Nikhil Duttaroy <54467400+Nikhil-Duttaroy@users.noreply.github.com>
2021-04-22 18:28:09 +0530

    added addCollectionAndDocuments function

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/firestore"; //importing firestore db
-import "firebase/auth";  //importing authentication
+import "firebase/auth"; //importing authentication
 
 const config = {
   apiKey: "AIzaSyBw85mnvtzocAy97XSMhxNsI5Iw1b5sgWY",
@@ -13,31 +13,45 @@ const config = {
 };
 
 //getting uid in firestoredb from google auth
-export const createUserProfileDocument=async (userAuth,additionalData)=> {
-  if(!userAuth) return; //if user not signed in then exit
-  
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return; //if user not signed in then exit
+
   const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-  const snapShot=await userRef.get()
+  const snapShot = await userRef.get();
   //exists shows if the snapshot of the user that signed in already exists in the db or not.
 
-  if(!snapShot.exists){    //if user already in db skip else run
-    const {displayName,email}=userAuth;
-    const createdAt=new Date();
+  if (!snapShot.exists) {
+    //if user already in db skip else run
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
 
-    try{
+    try {
       await userRef.set({
-        displayName,email,createdAt,...additionalData
-      })
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.error("Error Creating User", error.message);
     }
-    catch(error){
-      console.error('Error Creating Error',error.message);
-    }
-
   }
   return userRef;
-}
+};
 
+//To add data to firestore of products
+export const addCollectionAndDocuments = async(collectionKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();  
+    batch.set(newDocRef, obj)
+});
+  return await batch.commit()
+
+};
 
 // basic firestore and googleauth setup
 firebase.initializeApp(config);
