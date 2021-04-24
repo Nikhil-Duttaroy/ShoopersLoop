9917af2234ba461b5c6032facdbedd9419a4b718
Nikhil Duttaroy <54467400+Nikhil-Duttaroy@users.noreply.github.com>
2021-04-24 16:19:55 +0530

    implemented code to pull SHOP_Data from firestore and store in shop reducer and included HOC(withSpinner)

--- src/pages/Shop/ShopPage.components.jsx
+++ src/pages/Shop/ShopPage.components.jsx
@@ -1,25 +1,77 @@
-import React from 'react';
+import React from "react";
 import { Route } from "react-router-dom";
 
 import CollectionPage from "./../Collection/Collection.component";
 
-import CollectionsOverview from './../../components/CollectionsOverview/CollectionsOverview.components';
+import CollectionsOverview from "./../../components/CollectionsOverview/CollectionsOverview.components";
 // import { createStructuredSelector } from 'reselect';
 // import { selectCollections } from './../../redux/shop/shop.selector';
-// import { connect } from 'react-redux';
+import { connect } from "react-redux";
+import { updateCollections } from "./../../redux/shop/shop.actions";
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
+import WithSpinner from "../../components/WithSpinner/WithSpinner.component";
 
+const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
+class ShopPage extends React.Component {
+  state = {
+    loading: true,
+  };
 
-const ShopPage = ({ match }) => (
-  <div className='shop-page'>
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-);
+  unsubscribeFromSnapShot = null;
+
+  componentDidMount() {
+    const { updateCollections } = this.props;
+    const collectionRef = firestore.collection("collections");
+    // collectionRef.onSnapshot(async (snapshot) => {
+    //   const collectionMap = convertCollectionsSnapshotToMap(snapshot);
+    //   updateCollections(collectionMap);
+    //   this.setState({ loading: false });
+    // });
+  
+    collectionRef.get().then((snapshot) => {
+      const collectionMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionMap);
+      this.setState({ loading: false });
+    })
+  }
+
+  render() {
+    const { match } = this.props;
+    const { loading } = this.state;
+
+    return (
+      <div className='shop-page'>
+        <Route
+          exact
+          path={`${match.path}`}
+          render={(props) => (
+            <CollectionOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
+        <Route
+          exact
+          path={`${match.path}/:collectionId`}
+          render={(props) => (
+            <CollectionPageWithSpinner isLoading={loading} {...props} />
+          )}
+        />
+      </div>
+    );
+  }
+}
 
 // const mapStateToProps = createStructuredSelector({
 //   collections:selectCollections
 // })
 
+const mapDispatchToProps = (dispatch) => ({
+  updateCollections: (collectionMap) =>
+    dispatch(updateCollections(collectionMap)),
+});
 
-export default (ShopPage);
+export default connect(null, mapDispatchToProps)(ShopPage);
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -18,13 +18,14 @@ export const selectCollections = createSelector(
 
 export const SelectCollectionForPreview = createSelector(
   [selectCollections],
-  collections => Object.keys(collections).map(key=> collections[key])
-)
+  (collections) =>
+    collections ? Object.keys(collections).map((key) => collections[key]) : []
+);
 
 export const selectCollection = memoize((collectionUrlParam) =>
   createSelector(
     [selectCollections],
-    (collections) => collections[collectionUrlParam]
+    (collections) => (collections ? collections[collectionUrlParam] : null)
     //collections.find( collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
   )
 );
