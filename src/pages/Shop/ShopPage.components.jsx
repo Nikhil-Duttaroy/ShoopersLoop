0a134e5461af265562700ef1075e677a2726f751
Nikhil Duttaroy <54467400+Nikhil-Duttaroy@users.noreply.github.com>
2021-06-13 19:22:39 +0530

    conveted to hooks

--- src/pages/Shop/ShopPage.components.jsx
+++ src/pages/Shop/ShopPage.components.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect} from "react";
 import { Route } from "react-router-dom";
 
 import { connect } from "react-redux";
@@ -8,31 +8,26 @@ import  CollectionsOverviewContainer  from './../../components/CollectionsOvervi
 import CollectionsPageContainer from './../Collection/Collection.container';
 
 
-class ShopPage extends React.Component {
-  componentDidMount() {
-    const { fetchCollectionsStart } = this.props;
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+  useEffect(() => {
     fetchCollectionsStart();
-  }
+  }, [fetchCollectionsStart]);
 
-  render() {
-    const { match } = this.props;
-
-    return (
-      <div className='shop-page'>
-        <Route
-          exact
-          path={`${match.path}`}
-          component={CollectionsOverviewContainer}
-        />
-        <Route
-          exact
-          path={`${match.path}/:collectionId`}
-          component={CollectionsPageContainer}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className='shop-page'>
+      <Route
+        exact
+        path={`${match.path}`}
+        component={CollectionsOverviewContainer}
+      />
+      <Route
+        exact
+        path={`${match.path}/:collectionId`}
+        component={CollectionsPageContainer}
+      />
+    </div>
+  );
+};
 
 // const mapStateToProps = createStructuredSelector({
 //   collections:selectCollections
