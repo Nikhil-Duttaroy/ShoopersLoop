2815bba391a5f6591699adc3f4ccd5f88fe74349
Nikhil Duttaroy <54467400+Nikhil-Duttaroy@users.noreply.github.com>
2021-09-16 14:27:34 +0530

    category filter added

--- client/src/pages/Collection/Collection.component.jsx
+++ client/src/pages/Collection/Collection.component.jsx
@@ -3,12 +3,15 @@ import {
   CollectionPageContainer,
   CollectionTitle,
   CollectionItemsContainer,
+  TabContainer,
+  TabItem,
 } from "./Collection.styles.jsx";
 import { connect } from 'react-redux';
 import { selectCollection } from './../../redux/shop/shop.selector';
 import CollectionItem from './../../components/CollectionItem/CollectionItem.components';
 import FormInput from '../../components/FormInput/FormInput.components.jsx';
 import { FormInput1, FormInputContainer1 } from '../../components/CollectionItem/CollectionItem.styles.jsx';
+// import Tabs from '../../components/Tabs/Tabs.component.jsx';
 
 
 
@@ -16,43 +19,61 @@ const CollectionPage = ({ collection }) => {
   useEffect(() => {
     window.scrollTo(0, 0);
   });
-  
-  const [searchTerm,setSearchTerm] = useState("")
+
+  const [searchTerm, setSearchTerm] = useState("");
   const [rangeTerm1, setRangeTerm1] = useState(0);
   const [rangeTerm2, setRangeTerm2] = useState(2500);
   const [colourTerm, setColourTerm] = useState("");
-  
-  const [filterProducts, setFilterProducts] = useState([]);
-          
-if (rangeTerm2 == "") setRangeTerm2(2500);
-          
+  const [categoryTerm, setCategoryTerm] = useState("");
 
+  const [filterProducts, setFilterProducts] = useState([]);
+
+  if (rangeTerm2 === "") setRangeTerm2(2500);
 
   const { title, items } = collection;
-useEffect(() => {
-  setFilterProducts(
-    items
-      .filter((product) => {
-        return (
-          product.name.toLowerCase().includes(searchTerm.toLocaleLowerCase()) ||
-          product.description
+  // const {category} =items
+  //   const m=items.map((item)=> console.log(item))
+  //   console.log(m);
+  // const arr = items.filter(
+  //   (value, index) => value.category.indexOf(item.category) === index
+  // );
+   // const arr=items.filter((value) => value.category === "beanie")
+  // console.log(arr);
+  // console.log(arr);
+  const uniqueCategory = [...new Set(items.map((itm) => itm["category"]))];
+  const uniqueColour = [...new Set(items.map((itm) => itm["colour"]))];
+
+  useEffect(() => {
+    setFilterProducts(
+      items
+        .filter((product) => {
+          return (
+            product.name
+              .toLowerCase()
+              .includes(searchTerm.toLocaleLowerCase()) ||
+            product.description
+              .toLowerCase()
+              .includes(searchTerm.toLocaleLowerCase())
+          );
+        })
+        .filter((product) => {
+          return Number(product.price) >= Number(rangeTerm1);
+        })
+        .filter((product) => {
+          return Number(product.price) <= Number(rangeTerm2);
+        })
+        .filter((product) => {
+          return product.colour
             .toLowerCase()
-            .includes(searchTerm.toLocaleLowerCase())
-        );
-      })
-      .filter((product) => {
-        return Number(product.price) >= Number(rangeTerm1);
-      })
-      .filter((product) => {
-        return Number(product.price) <= Number(rangeTerm2);
-      })
-      .filter((product) => {
-        return product.colour
-          .toLowerCase()
-          .includes(colourTerm.toLocaleLowerCase());
-      })
-  );
-}, [searchTerm, rangeTerm1, rangeTerm2, colourTerm, items]);
+            .includes(colourTerm.toLocaleLowerCase());
+        })
+        .filter((product) => {
+          return product.category
+            .toLowerCase()
+            .includes(categoryTerm.toLocaleLowerCase());
+        })
+    );
+  }, [searchTerm, rangeTerm1, rangeTerm2, colourTerm, categoryTerm, items]);
 
   return (
     <>
@@ -94,43 +115,42 @@ useEffect(() => {
           />
 
           <datalist id='colour'>
-            {items.map((item, key) => (
-              <option key={key} value={item.colour} />
+            {uniqueColour.map((item, key) => (
+              <option key={key} value={item} />
             ))}
           </datalist>
         </FormInputContainer1>
+        <TabContainer>
+          <TabItem
+            onClick={(e) => {
+              setCategoryTerm("");
+            }}
+          >
+            ALL
+          </TabItem>
+          {
 
+            uniqueCategory.map((item) => (
+              <TabItem
+                onClick={(e) => {
+                  setCategoryTerm(item);
+                }}
+                
+              >
+                {item}
+              </TabItem>
+            ))
+          }
+        </TabContainer>
         <CollectionItemsContainer>
-          {/* {items */}
           {filterProducts.map((item) => (
             <CollectionItem key={item.id} item={item} />
           ))}
-          {/* {items
-            .filter((val) => {
-              if (searchTerm === "") {
-                return val;
-              } else if (
-                val.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
-              ) {
-                return val;
-              }
-            })
-            .filter((val1) => {
-              if (
-                val1.category.toLowerCase().includes(searchTerm1.toLocaleLowerCase())
-              ) {
-                // console.log(val1);
-                return val1;
-              }
-            })
-            .map((item) => (
-              <CollectionItem key={item.id} item={item} />
-            ))} */}
         </CollectionItemsContainer>
       </CollectionPageContainer>
     </>
   );
-};
+};;
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state),
